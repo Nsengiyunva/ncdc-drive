256d33dec4dd59eb559ee611f4b1147d89ab4a62
Nsengiyunva <kingbecks07@gmail.com>
2025-05-30 12:46:39 +0300

    updates the app

--- server/server.js
+++ server/server.js
@@ -21,6 +21,6 @@ app.get('/',(req,res)=>{
 app.use('/user',user)
 app.use('/ff',ffroute)
 
-app.listen(process.env.PORT || 8000,()=>{
+app.listen(process.env.PORT || 8500,()=>{
     console.log('server running');
 })
